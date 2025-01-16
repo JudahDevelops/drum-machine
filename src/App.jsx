724ba43a594be3cc94d59b6cd63a4834f5366231
JudahDevelops <judahdevelops@gmail.com>
2025-01-16 08:57:22 +0100

    im done with the project, everything is working however, if you switch the bank to the smooth piano kit and press the keys or the button it will show the right sound in the action-display but it will still play sounds from the heater kit, this is because i wasn't provided sounds for the smooth piano kit from freecodecamp and im too lazy to search or remove the bank feature entirely so i js left it like this

--- src/App.jsx
+++ src/App.jsx
@@ -22,19 +22,45 @@ class DrumMachine extends React.Component {
       volume: 50,
       bank: false
     }
+
+    this.audio = {
+      heater1: new Audio('src/sounds/Heater-1.mp3'),
+      heater2: new Audio('src/sounds/Heater-2.mp3'),
+      heater3: new Audio('src/sounds/Heater-3.mp3'),
+      heater4: new Audio('src/sounds/Heater-4_1.mp3'),
+      clap: new Audio('src/sounds/Clap.mp3'),
+      openHH: new Audio('src/sounds/OpenHH.mp3'),
+      kickHat: new Audio('src/sounds/Kick_n_Hat.mp3'),
+      kick: new Audio('src/sounds/RP4_KICK_1.mp3'),
+      closedHH: new Audio('src/sounds/ClosedHH.mp3')
+    }
+
     this.handleKeyDown = this.handleKeyDown.bind(this);
+    this.handleVolumeChange = this.handleVolumeChange.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener("keydown", this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.handleKeyDown);
   }
 
   handleVolumeChange = (event) => {
     this.setState({
       volume: event.target.value
     })
+
     const actionDisplay = document.getElementById('action-display');
     actionDisplay.innerHTML = `Volume: ${event.target.value}`;
-    audio.volume = event.target.value / 100;
+
+    Object.keys(this.audio).forEach(key => {
+      this.audio[key].volume = event.target.value / 100;
+    })
     
     setTimeout(() => {
-      actionDisplay.innerHTML = ''
+      actionDisplay.innerHTML = '';
     }, 1300);
   }
 
@@ -55,69 +81,100 @@ class DrumMachine extends React.Component {
   }
 
   handleclick = (event) => {
+
+    switch(event.target.id) {
+      case 'q':
+        this.audio.heater1.currentTime = 0;
+        this.audio.heater1.play();
+        break;
+      case 'w':
+        this.audio.heater2.currentTime = 0;
+        this.audio.heater2.play();
+        break;
+      case 'e':
+        this.audio.heater3.currentTime = 0;
+        this.audio.heater3.play();
+        break;
+      case 'a':
+        this.audio.heater4.currentTime = 0;
+        this.audio.heater4.play();
+        break;
+      case 's': 
+        this.audio.clap.currentTime = 0;
+        this.audio.clap.play();
+        break;
+      case 'd':
+        this.audio.openHH.currentTime = 0;
+        this.audio.openHH.play();
+        break;
+      case 'z':
+        this.audio.kickHat.currentTime = 0;
+        this.audio.kickHat.play();
+        break;
+      case 'x':
+        this.audio.kick.currentTime = 0;
+        this.audio.kick.play();
+        break;
+      case 'c':
+        this.audio.closedHH.currentTime = 0;
+        this.audio.closedHH.play();
+        break;
+      default:
+        break;
+    }
+
     const actionDisplay = document.getElementById('action-display');
-    actionDisplay.innerHTML = event.target.id
+    actionDisplay.innerHTML = event.target.value;
   }
-
-  componentDidMount() {
-    document.addEventListener("keydown", this.handleKeyDown);
-  }
-
-  componentWillUnmount() {
-    document.removeEventListener("keydown", this.handleKeyDown);
-  }
-
-  
-
 
   handleKeyDown = (event) => {
     const actionDisplay = document.getElementById('action-display');
-    const heater1 = new Audio('sounds/Heater-1.mp3');
-    const heater2 = new Audio('sounds/Heater-2.mp3');
-    const heater3 = new Audio('sounds/Heater-3.mp3');
-    const heater4 = new Audio('sounds/Heater-4_1.mp3');
-    const clap = new Audio('sounds/Clap.mp3');
-    const openHH = new Audio('sounds/OpenHH.mp3');
-    const kickHat = new Audio('sounds/Kick_n_Hat.mp3');
-    const kick = new Audio('sounds/RP4_KICK_1.mp3');
-    const closedHH = new Audio('soundns/ClosedHH.mp3');
 
     switch (event.key) {
       case 'q':
         actionDisplay.innerHTML = this.state.bank ? 'Chord 1' : 'Heater 1';
-        heater1.play()
+        this.audio.heater1.currentTime = 0;
+        this.audio.heater1.play();
         break;
       case 'w':
         actionDisplay.innerHTML = this.state.bank ? 'Chord 2' : 'Heater 2';
-        heater2.play()
+        this.audio.heater2.currentTime = 0;
+        this.audio.heater2.play();
         break;
       case 'e':
         actionDisplay.innerHTML = this.state.bank ? 'Chord 3' : 'Heater 3';
-        heater3.play()
+        this.audio.heater3.currentTime = 0;
+        this.audio.heater3.play();
         break;
       case 'a':
         actionDisplay.innerHTML = this.state.bank ? 'Shaker' : 'Heater 4';
-        heater4.play()
+        this.audio.heater4.currentTime = 0;
+        this.audio.heater4.play();
         break;
       case 's':
         actionDisplay.innerHTML = this.state.bank ? 'Open HH' : 'Clap';
-        clap.play()
+        this.audio.clap.currentTime = 0;
+        this.audio.clap.play();
         break;
       case 'd':
         actionDisplay.innerHTML = this.state.bank ? 'Closed HH' : 'Open HH';
-        openHH.play()
+        this.audio.openHH.currentTime = 0;
+        this.audio.openHH.play();
         break;
       case 'z':
         actionDisplay.innerHTML = this.state.bank ? 'Punchy Kick' : 'Kick  n\' Hat';
-        kickHat.play()
+        this.audio.kickHat.currentTime = 0;
+        this.audio.kickHat.play();
         break;
       case 'x':
         actionDisplay.innerHTML = this.state.bank ? 'Side Stick' : 'Kick';
-        kick.play()
+        this.audio.kick.currentTime = 0;
+        this.audio.kick.play();
         break;
       case 'c':
         actionDisplay.innerHTML = this.state.bank ? 'Snare' : 'Closed HH';
-        closedHH.play()
+        this.audio.closedHH.currentTime = 0;
+        this.audio.closedHH.play();
         break;
       default:
         break;
@@ -136,19 +193,19 @@ class DrumMachine extends React.Component {
       <div id="controls-container">
         <div id='buttons'>
           <div class='row'>
-            <button onClick={this.handleclick} id={this.state.bank ? 'Chord 1' : 'Heater 1'} disabled={!this.state.power}>Q</button>
-            <button onClick={this.handleclick} id={this.state.bank ? 'Chord 2' : 'Heater 2'} disabled={!this.state.power}>W</button>
-            <button onClick={this.handleclick} id={this.state.bank ? 'Chord 3' : 'Heater 3'} disabled={!this.state.power}>E</button>
+            <button onClick={this.handleclick} value={this.state.bank ? 'Chord 1' : 'Heater 1'} disabled={!this.state.power} id='q'>Q</button>
+            <button onClick={this.handleclick} value={this.state.bank ? 'Chord 2' : 'Heater 2'} disabled={!this.state.power} id='w'>W</button>
+            <button onClick={this.handleclick} value={this.state.bank ? 'Chord 3' : 'Heater 3'} disabled={!this.state.power} id='e'>E</button>
           </div>
           <div class='row'>
-            <button onClick={this.handleclick} id={this.state.bank ? 'Shaker' : 'Heater 4'} disabled={!this.state.power}>A</button>
-            <button onClick={this.handleclick} id={this.state.bank ? 'Open HH' : 'Clap'} disabled={!this.state.power}>S</button>
-            <button onClick={this.handleclick} id={this.state.bank ? 'Closed HH' : 'Open HH'} disabled={!this.state.power}>D</button>
+            <button onClick={this.handleclick} value={this.state.bank ? 'Shaker' : 'Heater 4'} disabled={!this.state.power} id='a'>A</button>
+            <button onClick={this.handleclick} value={this.state.bank ? 'Open HH' : 'Clap'} disabled={!this.state.power} id='s'>S</button>
+            <button onClick={this.handleclick} value={this.state.bank ? 'Closed HH' : 'Open HH'} disabled={!this.state.power} id='d'>D</button>
           </div>
           <div class='row'>
-          <button onClick={this.handleclick} id={this.state.bank ? 'Punchy Kick' : "Kick n' Hat"} disabled={!this.state.power}>Z</button>
-            <button onClick={this.handleclick} id={this.state.bank ? 'Side Stick' : 'Kick'} disabled={!this.state.power}>X</button>
-            <button onClick={this.handleclick} id={this.state.bank ? 'Snare' : 'Closed HH'} disabled={!this.state.power}>C</button>
+          <button onClick={this.handleclick} value={this.state.bank ? 'Punchy Kick' : "Kick n' Hat"} disabled={!this.state.power} id='z'>Z</button>
+            <button onClick={this.handleclick} value={this.state.bank ? 'Side Stick' : 'Kick'} disabled={!this.state.power} id='x'>X</button>
+            <button onClick={this.handleclick} value={this.state.bank ? 'Snare' : 'Closed HH'} disabled={!this.state.power} id='c'>C</button>
           </div>
         </div>
         <div id='controls'>
